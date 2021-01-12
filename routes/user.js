827212c6ee24ef703ca51eb56cfc477b50c1750b
DavidMendoza5 <cruz-azul99@hotmail.com>
2021-01-12 10:14:13 -0600

    Middleware verifyData added to crearUsuario/

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const userControllers = require('../controllers/user')
 const verifyUser = require('../middlewares/verifyUsers')
+const verifyData = require('../middlewares/verifyData')
 
 const api = express.Router()
 
-api.post('/crearUsuario', verifyUser, userControllers.crearUsuario)
+api.post('/crearUsuario', verifyData, verifyUser, userControllers.crearUsuario)
 api.get('/obtenerUsuarios', userControllers.getUser)
 api.get('/obtenerUsuario/busqueda/:nombre', userControllers.getUserByName)
 api.get('/obtenerUsuario/:userName', userControllers.getUserByUserName)
