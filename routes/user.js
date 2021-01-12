c8c485d46835394df90b3171e523a3fdd107dbb4
DavidMendoza5 <cruz-azul99@hotmail.com>
2021-01-12 09:47:15 -0600

    Routes modified and added - A route to serch users by name was modified, now you have to add /busqueda and the name of the user - Another get was added, the new route is obtenerUsuario/:userName and this route reply with one user

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const api = express.Router()
 
 api.post('/crearUsuario', verifyUser, userControllers.crearUsuario)
 api.get('/obtenerUsuarios', userControllers.getUser)
-api.get('/obtenerUsuario/:nombre', userControllers.getUserByName)
+api.get('/obtenerUsuario/busqueda/:nombre', userControllers.getUserByName)
+api.get('/obtenerUsuario/:userName', userControllers.getUserByUserName)
 
 module.exports = api
