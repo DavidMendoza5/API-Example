31ae220f39d0c65b0e46e675a4b53d2b204dc541
DavidMendoza5 <cruz-azul99@hotmail.com>
2021-01-12 09:52:58 -0600

    Name changed by userName - The function now search for a userName instead of a name, the user names can not bre repeated

--- middlewares/verifyUsers.js
+++ middlewares/verifyUsers.js
@@ -2,7 +2,7 @@ const {getUserOnDB} = require('../database/users/user')
 
 const verifyUser = async (req, res ,next) => {
     try {
-        let userRepeated = await getUserOnDB({nombre:req.body.nombre}, next)
+        let userRepeated = await getUserOnDB({userName:req.body.userName}, next)
         if(userRepeated.length > 0) {
             throw new Error("Usuario duplicado")
         }
